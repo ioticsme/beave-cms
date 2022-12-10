7f61f9388ecc66dd3782659cbe3353fe32b4448a
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2022-12-10 16:14:22 +0530

    package initiation

--- custom/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-require('express-group-routes')
-const router = express.Router()
-
-router.get('/test-custom-route', async(req, res) => {
-    res.send('This is custom test route...!')
-})
-
-module.exports = router
