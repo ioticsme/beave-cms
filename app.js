3ed895235bfd91d861530f477c1117e32f3bb002
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2023-02-27 22:27:43 +0530

    version upgrade

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const nunjucks = require('nunjucks')
 const app = express()
 
-// const application = require('./application')
+// const application = require('../cms-package')
 const application = require('@ioticsme/beave-cms')
 
 app.use(express.static('public'))
 
 app.use(application)
+
+// Setting template engine (nunjucks)
+nunjucks.configure('views', {
+    express: app,
+    autoescape: true,
+})
+app.set('view engine', 'njk')
 
 // app.get('/admin/config/content-types', async(req, res) => {
 //     return res.send('test')
